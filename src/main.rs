ea658d5a940516fcc3bb4f1c55924612aa581268
Johannes Schwarzbart <schwarzbartj@gmail.com>
2020-09-03 17:42:43 +0200

    add 'Delete' and 'Display' capabilites for Bookie

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ use rusqlite::Result;
 mod bookmark;
 mod database;
 mod parser;
-use std::fmt;
+use std::{fmt, io};
 use structopt::StructOpt;
-use Bookie::{Add, Delete, Update};
+use Bookie::{Add, Delete, Display, Update};
 fn main() -> Result<(), Error> {
     let db = DB::open(false);
 
     let input = Bookie::from_args();
 
     match input {
+        Display {} => {
+            db.display_bookmarks();
+        }
+
         Add {
             url,
             title,
@@ -21,11 +25,31 @@ fn main() -> Result<(), Error> {
         } => {
             db.add_bookmark(&title, &url, &notes, &tags)?;
         }
-        Delete {} => println!("Delete functionality not yet implemented"),
+        Delete { id } => {
+            let mut input = String::new();
+
+            if db.bookmark_exists(id) {
+                println!("Are you sure you want to delete the following bookmark?");
+                db.display_selected_bookmark(id);
+                println!("y/n");
+                io::stdin()
+                    .read_line(&mut input)
+                    .expect("Couldn't read line");
+
+                match input.as_str() {
+                    "y\n" => {
+                        db.delete_bookmark(id);
+                        println!("Deleted bookmark with id {}", id)
+                    }
+                    "n\n" => println!("Bookmarks was NOT deleted."),
+                    _ => println!("Bookmark was NOT deleted."),
+                }
+            } else {
+                println!("The bookmark with id {} does not exist!", id);
+            }
+        }
         Update {} => println!("Update functionality not yet implemented"),
     };
-
-    db.display_bookmarks();
 
     Ok(())
 }
